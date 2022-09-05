c14f34a7fc2bc40145a6c6575280ead4fe6f2390
Jacob Taylor <jjakettaylor1993@hotmail.com>
2022-09-05 12:12:30 +0100

    added the ste up of the database which was forgotten, error persists

--- scripts/create-database.js
+++ scripts/create-database.js
@@ -11,3 +11,32 @@ require("dotenv").config({
 });
 
 const {DB_PASSWORD, DB_NAME, DB_USER, DB_HOST,DB_PORT} = process.env
+
+const setUpDatabase = async () => {
+    try {
+  
+      const db = await mysql.createConnection({
+        host: DB_HOST,
+        user: DB_USER,
+        password: DB_PASSWORD,
+        port: DB_PORT,
+      });
+      await db.query(` CREATE DATABASE IF NOT EXISTS ${DB_NAME}`);
+      await db.query(`USE ${DB_NAME}`)
+  
+    } catch (err) {
+      console.log(
+        `Your environment variables might be wrong. Please double check .env file`
+      );
+      console.log('Environment Variables are:', {
+        DB_PASSWORD,
+        DB_NAME,
+        DB_USER,
+        DB_HOST,
+        DB_PORT,
+      });
+      console.log(err);
+    }
+  };
+  
+  setUpDatabase();
